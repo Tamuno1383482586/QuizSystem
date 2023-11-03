d1baa2ae155ff5012e2698a6477c4ae31b454d00
tamunodivine8@gmail.com <tamunodivine8@gmail.com>
2023-11-03 09:53:38 +0100

    Building of Javascript modules to generate questions,generate options,next and previous button functionality,submitQuiz Module to handle submitting

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var questions = {};
+var questionCount = 0;
+
 //Fetch Api and Promise to read JSON data
 let fetchRequest = fetch("questions.json");
 
@@ -7,29 +9,141 @@ fetchRequest
     return response.json();
   })
   .then((question) => {
-    var result = renderQuestions(question);
+    var optionsArrayLen = question[questionCount].options.length;
 
-    document
-      .querySelector(".questions")
-      .insertAdjacentHTML("beforeend", result);
-      var options = renderQuestionsOptions(question,4)
-document.querySelector(".options").appendChild(options);
+
+
+    nextAndPreviousButtonFunctionality(question, optionsArrayLen);
+    renderQuestions(question, questionCount)
+    checkForCorrectAnswer()
+    renderQuestionsOptions(question, optionsArrayLen, questionCount)
+
+
+
+
   })
   .catch((error) => {
     console.log(error);
   });
 
-var renderQuestions = (question) => {
-  return `<p class="question">${question[0].question}</p>`;
-};
+function renderQuestions(question, questionNumber) {
+  var questionJSON = `
+<p class="question">${question[questionNumber].question}</p>
+`;
+  document.querySelector('.questions').innerHTML = questionJSON;
 
-function renderQuestionsOptions(question,noOfOptions) {
-  var input = document.createElement('input');
-  input.type = "radio";
-  input.name = "radioElem";
-  input.id = "radioElem";
-  
-  
- return input; 
+}
+
+function renderQuestionsOptions(question, noOfOptionsLen, questionNumber) {
+
+  for (let i = 0; i < noOfOptionsLen; i++) {
+    var alphabet = String.fromCharCode(97 + i)
+    var optionLabelText = question[questionNumber].options[i][`${alphabet}`];
+
+    var input = document.createElement('input');
+    input.type = "radio";
+    input.name = "radioElem";
+    input.id = "radioElem " + i;
+    input.value = question[questionNumber].options[i]['correct'];
+    var breaker = document.createElement('br')
+    var label = document.createElement('label');
+
+    label.htmlFor = "radioElem " + i;
+    var alphabet = String.fromCharCode(97 + i)
+    var optionLabelText = question[questionNumber].options[i][`${alphabet}`];
+
+    //console.log(optionLabelText);
+    //  console.log(alphabet);
+    label.appendChild(document.createTextNode(optionLabelText));
+
+
+  //  var inputGroup = input + label + breaker;
+
+
+    document.querySelector('.options').append(input);
+    document.querySelector(".options").append(label);
+    document.querySelector(".options").append(breaker);
+  }
+}
+
+function checkForCorrectAnswer() {
+  var correctAnswerCount = 0;
+  var incorrectAnswerCount = 0;
+  var optionsParent = document.querySelector('.options');
+  optionsParent.addEventListener('change', (e) => {
+
+    e.stopImmediatePropagation();
+    if (e.target.getAttribute("value") == "true") {
+
+      correctAnswerCount = correctAnswerCount + 1;
+      console.log("Correct Count : " + correctAnswerCount);
+    } else {
+
+      incorrectAnswerCount = incorrectAnswerCount + 1;
+      console.log("Incorrect Count : " + incorrectAnswerCount);
+    }
+  });
+  optionsParent.removeEventListener('change', null);
+
+}
+
+//checkForCorrectAnswer()
+function nextAndPreviousButtonFunctionality(question, optionsArrayLen) {
+
+  var prevButton = document.querySelector('#prev');
+  var nextButton = document.querySelector('#next')
+  var controlButtonsParent = document.querySelector('.control-buttons');
+
+
+  controlButtonsParent.addEventListener('click', (e) => {
+    e.stopImmediatePropagation();
+    e.preventDefault();
+
+
+
+    if (e.target.id == "next" && questionCount < question.length - 1) {
+
+      questionCount += 1;
+      document.querySelector('.options').innerHTML = '';
+      renderQuestions(question, questionCount)
+
+      renderQuestionsOptions(question, optionsArrayLen, questionCount)
+      checkForCorrectAnswer()
+    } else if (e.target.id == "prev" && questionCount > 0) {
+      questionCount -= 1;
+      document.querySelector('.options').innerHTML = '';
+      renderQuestions(question, questionCount)
+
+      renderQuestionsOptions(question, optionsArrayLen, questionCount)
+      checkForCorrectAnswer()
+
+    }
+    prevButton.disabled = questionCount === 0;
+    nextButton.disabled = questionCount === question.length - 1;
+
+
+
+  });
+  controlButtonsParent.removeEventListener('click', null);
+}
+
+
+function submitQuiz([correctCount, incorrectCount]) {
+
+  var submitButton = document.querySelector('input[type = "submit"]')
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert(`Correct Answers : ${correctCount} \n
+    IncorrectAnswers : ${incorrectCount}
+    `)
+  });
+  submitButton.removeEventListener('click', null);
+}
+
+
+
+
+function rememberLastOptionClicked(question) {
   
 }
